6fc3adff945a4fa1909efa78a0534f1b86813a2a
Bakhtiarov Sergey <bakhtiarov.sergey@gmail.com>
2020-01-14 20:32:42 +0900

    shortened version

--- lesson-02/4. split_whitespace.py
+++ lesson-02/4. split_whitespace.py
@@ -1,7 +1,4 @@
-user_string = input('Введите слова, разделяя их пробелом\n')
-user_list = user_string.split(' ')
-list_len = len(user_list)
+user_list = input('Введите слова, разделяя их пробелом\n').split(' ')
 
-for item in range(list_len):
-    item_shorted = user_list[item][:10]
-    print (f'{item + 1}. {item_shorted}')
+for item in range(len(user_list)):
+    print (f'{item + 1}. {user_list[item][:10]}')
