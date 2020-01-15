a2dc6dde57b82076545c1da09bfd62bc7ea9fba6
Bakhtiarov Sergey <bakhtiarov.sergey@gmail.com>
2020-01-15 20:28:45 +0900

    Задания 3-го урока

---
+++ lesson-03/4. exponentiation.py
@@ -0,0 +1,15 @@
+def exponentiation(x, y):
+    """Returns a negative power of a number"""
+    return x**y
+
+def exponentiation_cycle(x, y):
+    """Returns a negative power of a number"""
+    res = x
+    for i in range(y + 1, 0):
+        res *= x
+    return (1 / res)
+
+x = 2.5
+y = -9
+print(f'Result = {exponentiation(x, y)}')
+print(f'Result_cycle = {exponentiation_cycle(x, y)}')
